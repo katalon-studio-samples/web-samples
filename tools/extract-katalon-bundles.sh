ffa764ae2ec774264ef6455a2e4f6f94fe8a4f29
Huy Nguyen <huynguyen@FVFY81QKHV2H.local>
2020-01-16 22:52:30 +0700

    update

--- tools/extract-katalon-bundles.sh
+++ tools/extract-katalon-bundles.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
+JARS_DIR="$1"
+OUTPUT_DIR="$2"
+
 extract () {
   jar_file="$1"
-  jar_dir="${OUTPUT_DIR}/${1%.jar}"
-  unzip -d "$jar_dir" "$jar_file"
+  extract_dir="$OUTPUT_DIR/${1%.jar}"
+  unzip -d "$extract_dir" "$jar_file"
+
+  if [ -d "$extract_dir"/resources ]; then rm -rf "$extract_dir"/resources; fi
+  if [ -d "$extract_dir"/resource ]; then rm -rf "$extract_dir"/resource; fi
 }
 
-BINARIES_DIR="$1"
+rm -rf "$OUTPUT_DIR"/*
 
-OUTPUT_DIR="$2"
-rm -rf "${OUTPUT_DIR}"/*
-
-cd "${BINARIES_DIR}"
+cd "$JARS_DIR"
 for jar in $(ls ./*katalon*.jar)
 do
-  extract ${jar}
+  extract $jar
 done
+
+cd $OUTPUT_DIR
+rm com.kms.katalon.execution.webservice*/com/kms/katalon/execution/webservice/contribution/WebServiceConfigurationContributor.class
