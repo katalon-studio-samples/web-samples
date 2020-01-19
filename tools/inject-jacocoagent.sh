1659302ef1e5f16a4ebf2789a0e4f6db105c2b8e
Huy Nguyen <huynguyen@FVFY81QKHV2H.local>
2020-01-19 18:04:56 +0700

    update

--- tools/inject-jacocoagent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PROJECT_SETTINGS_DIR="$1"
-JACOCOAGENT="$2"
-REPORT_FILE="$3"
-LAUNCH_PROPERTY="execution.launch.vmArgs=\"-javaagent\:$JACOCOAGENT\=destfile\=$REPORT_FILE\""
-
-cd $PROJECT_SETTINGS_DIR
-echo $LAUNCH_PROPERTY > "com.kms.katalon.execution.setting.properties" 
